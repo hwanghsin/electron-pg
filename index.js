1952a12d8be9f7b05cd3844f8de26c3bf618ef6d
Ted Hwang <ted.hwang@mitake.com.tw>
2025-01-14 18:48:38 +0800

    exec script for test

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const { app, BrowserWindow, Notification, ipcMain } = require("electron");
 // const path = require("path");
+const { exec } = require("child_process");
+
+function checkNotifications() {
+  exec(
+    `osascript -e 'tell application "System Events" to get enabled of every item of (get every application process whose bundle identifier is "com.your.app.bundleid")'`,
+    (error, stdout) => {
+      if (error) {
+        console.error("Error checking macOS notifications:", error);
+      } else {
+        console.log("Notification status:", stdout.trim());
+      }
+    }
+  );
+}
+
+const openNotifySetting = (device = "darwin") => {
+  const isMac = device === "darwin";
+
+  exec(
+    isMac
+      ? `open "x-apple.systempreferences:com.apple.preference.notifications"`
+      : "start ms-settings:notifications",
+    (err) => {
+      if (err) {
+        console.error("Failed to open notification settings:", err);
+      } else {
+        console.log("Notification settings opened successfully.");
+      }
+    }
+  );
+};
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -12,8 +43,8 @@ const createWindow = () => {
     },
   });
 
-  ipcMain.on("MESSAGE", (event, arg) => {
-    console.log(arg);
+  ipcMain.on("MESSAGE", (event, message) => {
+    console.log("message", message);
   });
 
   ipcMain.handle("ASYNC_ACTION", async (event, message) => {
@@ -24,16 +55,38 @@ const createWindow = () => {
     new Notification({ title: "Notification", body: arg }).show();
   });
 
+  ipcMain.on("DEVICE", () => {
+    const platform = process.platform;
+    console.log("DEVICE", platform);
+    // openNotifySetting(platform);
+  });
+
   win.loadFile("index.html");
 };
 
-app.whenReady().then(() => {
-  createWindow();
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  })
+  .then(() => {
+    // checkNotifications();
+    // console.log("checkNotifications");
+    // const notification = new Notification({
+    //   title: "Enable Notifications",
+    //   body: "Notifications are disabled. Click here to enable them in system preferences.",
+    //   actions: [{ type: "button", text: "Open Settings" }],
+    // });
+    // notification.on("action", () => {
+    //   const platform = process.platform;
+    //   openNotifySetting(platform);
+    // });
+    // notification.show();
   });
-});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 const ipcRenderer = require("electron").ipcRenderer;
 const notify = document.getElementById("notify");
+const device = document.getElementById("device");
 
 notify.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,4 +11,11 @@ notify.addEventListener("submit", (event) => {
   //   new Notification("Hello from browser", {
   //     body: "option body",
   //   }).show();
+});
+
+device.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // console.log(Notification.permission);
+  ipcRenderer.send("DEVICE");
 });
